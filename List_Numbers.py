e70ebb1b308b67803448e695d39823d87487bb27
Yash Bharwad <58401798+yash1782@users.noreply.github.com>
2022-03-06 19:55:25 +0530

    Add files via upload

---
+++ List_Numbers.py
@@ -0,0 +1,11 @@
+list1 = [12, -7, 5, 64, -14]
+print(list1)
+for j in list1:
+    if int(j)>0:
+        print(j)
+list2 = [12, 14, -95, 3]
+print(list2)
+for i in list2:
+    if int(i)>0:
+        print(i)
+                
